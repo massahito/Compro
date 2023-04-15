8b95b8f1abb3ef694516ea091b46a9cd2cbb9c7d
MasaDevs <MasaDevs@gmail.com>
2023-04-15 20:10:39 +0900

    ok

---
+++ abc296/e.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int	A, N;
+	vector<int>	vct;
+	cin >> N;
+	for(int i = 0; i < N; i++)
+	{
+		cin >> A;
+		vct.push_back(A);
+	}
+
+
+
+}
