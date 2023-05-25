7ad5aa68e5a46e8234f423e94e31bcf7e59d853c
MasaDevs <masadevs@gmail.com>
2023-05-26 01:00:52 +0900

    ok

---
+++ brown/200C.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+	string S;
+
+	cin >> S;
+	for(int i = 0; i < 10000; i++)
+	{
+		int num	= 1; 	
+		string C = S;
+
+		for(int j = 0; j < 4; j++)
+		{
+			if(C[num] ==)	
+		}
+}
